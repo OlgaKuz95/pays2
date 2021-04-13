976000103523a52ea2afe36159f50faaa95127a6
Olga Kuzevanova <cuzevanova.olg@yandex.ru>
2021-04-14 01:42:48 +0700

    добавлены функции с типами карт

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,65 @@
 package  ru.netology
 
 fun main() {
-    // val itemFeeMin = 3_500
-    val itemFee = 0.75
-    val itemPercent = 100
-    val moneyTransfer = 5_000_000
-    val transferMonth = 0
-    val moneyTransfermax = 1_500_000
-    val fee = moneyTransfer * itemFee / itemPercent
-    val vkPay = moneyTransfer
-    val amount = fee + moneyTransfer
-    val result = when (fee) {
 
-        //vkPay -> "$moneyTransfer"
-        else ->
+    val result =vkPay()
+    println("$result")
+
+}
+fun vkPay(
+    transferMonth:Double = 0.0,
+    itemFeeone: Double=0.0,
+    moneyTransfermaxDay: Double = 1_500_000.0,
+    moneyTransfer:Double = 5_000.0,
+    moneyTransfermaxMonth: Double =4_000_000.0
+
+){
+    val moneyMonth = moneyTransfer+transferMonth
+    val amount = moneyTransfer+itemFeeone
+    val result = when{
+        (moneyTransfer >moneyTransfermaxDay)-> print ("превышен дневной  лимит")
+        (moneyMonth>moneyTransfermaxMonth)->print("превышен месячный лимит")
+        else-> print (amount)
     }
-    //val result = if (fee <= itemFeeMin )moneyTransfer + itemFeeMin else amount
-    println("amount: $result")
+}
+fun visaMir(
+    transferMonth:Double = 0.0,
+    itemFeetwo: Double=0.75,
+    itemPercent: Double = 100.0,
+    itemFeemin:Double = 350.0,
+    moneyTransfermaxDay: Double = 15_000_000.0,
+    moneyTransfer:Double = 5_000_000.0,
+    moneyTransfermaxMonth: Double =60_000_000.0
+){
+val amount = moneyTransfer*itemFeetwo/itemPercent
+    val moneyMonth = moneyTransfer + transferMonth
+    val result =when{
+        moneyTransfer >moneyTransfermaxDay-> print ("превышен дневной  лимит")
+        moneyMonth>moneyTransfermaxMonth->print("превышен месячный лимит")
+        amount>itemFeemin ->print (moneyTransfer+itemFeemin)
+        else->print (amount)
+    }
+}
+fun MastercardMaestro(
+    transferMonth:Double = 0.0,
+    itemPercent: Double = 100.0,
+    itemFeethree: Double= 0.6,
+    fixedСonver: Double = 2_000.0,
+    moneyTransferperiodOne: Double = 30_000.0,
+    moneyTransferperiodTwo:Double = 7_500_000.0,
+    moneyTransfermaxDay: Double =  15_000_000.0,
+    moneyTransfer:Double = 16_000_000.0,
+    moneyTransfermaxMonth: Double =60_000_000.0
 
+){
+    val amount = moneyTransfer*itemFeethree/itemPercent+fixedСonver
+    val moneyMonth = moneyTransfer + transferMonth
+    val result =when{
+        moneyTransfer >moneyTransfermaxDay->  print ("превышен дневной  лимит")
+        moneyMonth>moneyTransfermaxMonth->print("превышен месячный лимит")
+        moneyTransfer >= moneyTransferperiodOne&& moneyTransfer<= moneyTransferperiodTwo ->print (moneyTransfer)
+        else -> print (amount)
+    }
+
+    
 }
